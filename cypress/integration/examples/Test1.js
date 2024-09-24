394b36aed4b3391a33a6d3b5162d3897ba69a8a2
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-24 22:25:33 +0530

    Navigating to URL - First Test Case

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,13 @@
+//Cypress - Spec
+
+describe('My First Test Suite', function()
+{
+    it('My First Test Case', function(){
+        //test cases
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    })
+
+    it('My Second Test Case', function(){
+
+    })
+})
