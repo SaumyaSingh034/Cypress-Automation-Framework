007b943b56b1a29daefa66eb16fbff01b9b2a41a
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-24 22:58:34 +0530

    Locators in Cypress

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -5,6 +5,7 @@ describe('My First Test Suite', function()
     it('My First Test Case', function(){
         //test cases
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca');
     })
 
     it('My Second Test Case', function(){
